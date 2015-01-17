63ed71b63ff381930866faef3d63af44056c0766
Ryan <ryan@barnett.io>
2015-01-17 00:25:37 -0500

    Refactor

--- p3.py
+++ p3.py
@@ -2,33 +2,38 @@ __author__ = 'Ryan'
 '''The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?'''
-
-n = 13195
+import math
+n = 600851475143
 num_list = []
 prime_list = []
 
-largest_prime = 0
+#### Overflow error, range too large for memory
+# for num in range(2, n):
+#     if n % num == 0:
+#         print 'current number that can be divided into the input:', num
+#         for nums in range(1, num+1):
+#             print 'checking if', num, 'is divisble by', nums
+#             if num % nums == 0:
+#                 num_list.extend([nums])
+#                 print 'adding', nums, 'to the temporary number list'
+#                 print 'temporary number list:', num_list
+#         if len(num_list) == 2:
+#             prime_list.extend([num])
+#             print 'prime list extended', prime_list
+#             print num
+#         num_list = []
+#
+# print 'max prime:', max(prime_list)
 
-#while largest_prime == 0:
-
-for num in range(2, n):
+for num in range(2, int(math.sqrt(n))+2):
     if n % num == 0:
-        print 'current number that can be divided into the input:', num
         for nums in range(1, num+1):
-            print 'checking if', num, 'is divisble by', nums
             if num % nums == 0:
                 num_list.extend([nums])
-                print 'adding', nums, 'to the temporary number list'
-                print 'temporary number list:', num_list
         if len(num_list) == 2:
             prime_list.extend([num])
-            print 'prime list extended', prime_list
-            print num
         num_list = []
-
-print 'max prime:', max(prime_list)
-
-
+print 'Max prime:', max(prime_list)
 
 
 
